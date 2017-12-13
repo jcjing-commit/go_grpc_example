d6bc17dc65df63ad6f18e7842d510056f7189fc6
xingchen20133 <892947178@qq.com>
2017-12-13 16:54:09 +0800

    up

--- src/client/client.go
+++ src/client/client.go
@@ -21,7 +21,7 @@ func main() {
     c := calc.NewCalcClient(conn)
 
     // Contact the server and print out its response.
-    // Add test with 12 13 
+    // Add test with 12 13 and 25 
     var num1, num2 int64
     num1 = 12
     num2 = 13
@@ -31,7 +31,7 @@ func main() {
     }else{
         log.Printf("grpc Add: %v + %v = %v",num1,num2,addRulst.Result)
     }
-    // Sub test with 8,2
+    // Sub test with 8,2 and 6
     num1 = 8
     num2 = 2
     subRulst,err := c.Sub(context.Background(), &calc.SubRequest{Num1:num1,Num2:num2})
@@ -40,7 +40,7 @@ func main() {
     }else{
         log.Printf("grpc Sub: %v - %v = %v",num1,num2,subRulst.Result)
     }
-   //Mult test with 5,2 
+   //Mult test with 5,2 and 10 
     num1 = 5 
     num2 = 2
     multRulst,err := c.Mult(context.Background(), &calc.MultRequest{Num1:num1,Num2:num2})
@@ -49,7 +49,7 @@ func main() {
     }else{
 	log.Printf("grpc Mult: %v * %v = %v",num1,num2,multRulst.Result)
     }
-    //Div test with 24 2
+    //Div test with 24 2 and 12
     num1 = 24
     num2 = 2
     divRulst,err := c.Div(context.Background(), &calc.DivRequest{Num1:num1,Num2:num2})

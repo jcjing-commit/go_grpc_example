0a9526ddf52583906f5ec32a171b14d2f7ea737f
xingchen20133 <892947178@qq.com>
2017-12-14 18:11:52 +0800

    up

--- readme.txt
+++ readme.txt
@@ -1,10 +1,9 @@
 export GOPATH=$(pwd) 或 (export GOPATH=.../go_grpc_example)
 go get -u github.com/golang/protobuf/{proto,protoc-gen-go}    	   //需要到GOPATH中执行
 go get -u google.golang.org/grpc                             	   //工作目录中执行(src中)
-protoc --go_out=plugins=grpc:./输出目录／ ./*.proto文件所在目录    //将$GOPATH/bin添加到 PATH执行才不会报错（自动生成服务器和客户端的注册函数）
-(如果用$ protoc --go_out=./ ./proto/helloworld.proto生成*.pb.go需要自己写服务器和客户端的注册函数）
+protoc --go_out=plugins=grpc:./ ./calc/*.proto文件所在目录    //将$GOPATH/bin添加到 PATH执行才不会报错（自动生成服务器和客户端的注册函数）
+(如果用$ protoc --go_out=./ .../*.proto生成*.pb.go需要自己写服务器和客户端的注册函数）
 
-将生成的proto/*.pb.go文件拷贝到相应的文件夹里(./calc)
 =================================================================================================
 不想自己操作以上过程可以执行：
 ./tool.sh     //重新下载开源库有点慢
--- src/server/server.go
+++ src/server/server.go
@@ -1,56 +1,71 @@
 package main
 
 import (
-    "log"
-    "net"
-    "golang.org/x/net/context"
-    "google.golang.org/grpc"
-    "calc"
-    "google.golang.org/grpc/reflection"
-    "fmt"
-    "errors"
+	"calc"
+	"errors"
+	"fmt"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"io"
+	"log"
+	"net"
 )
 
 const (
-    port = ":50051"
+	port = ":50051"
 )
 
 // server is used to implement calc.
 type server struct{}
 
-func (s *server) Add(ctx context.Context, in *calc.AddRequest) (*calc.AddReply, error){
-	fmt.Println("%v + %v = %v",in.Num1,in.Num2,in.Num1+in.Num2)
-	return &calc.AddReply{Result:(in.Num1+in.Num2)}, nil
+func (s *server) Add(ctx context.Context, in *calc.AddRequest) (*calc.AddReply, error) {
+	fmt.Println("%v + %v = %v", in.Num1, in.Num2, in.Num1+in.Num2)
+	return &calc.AddReply{Result: (in.Num1 + in.Num2)}, nil
 }
 
-func (s *server) Sub(ctx context.Context, in *calc.SubRequest) (*calc.SubReply, error){
-	fmt.Println("%v - %v = %v", in.Num1, in.Num2, in.Num1 - in.Num2)
-	return &calc.SubReply{Result:(in.Num1 - in.Num2)}, nil
+func (s *server) Sub(ctx context.Context, in *calc.SubRequest) (*calc.SubReply, error) {
+	fmt.Println("%v - %v = %v", in.Num1, in.Num2, in.Num1-in.Num2)
+	return &calc.SubReply{Result: (in.Num1 - in.Num2)}, nil
 }
 
-func (s *server) Mult(ctx context.Context, in *calc.MultRequest) (*calc.MultReply, error){
-        fmt.Println("%v * %v = %v",in.Num1,in.Num2,in.Num1*in.Num2)
-        return &calc.MultReply{Result:(in.Num1*in.Num2)}, nil
+func (s *server) Mult(ctx context.Context, in *calc.MultRequest) (*calc.MultReply, error) {
+	fmt.Println("%v * %v = %v", in.Num1, in.Num2, in.Num1*in.Num2)
+	return &calc.MultReply{Result: (in.Num1 * in.Num2)}, nil
 }
 
-func (s *server) Div(ctx context.Context, in *calc.DivRequest) (*calc.DivReply, error){
-    	if 0 == in.Num2{
-		return nil,errors.New("divisor is 0") 
-	} 
-        fmt.Println("%v / %v = %v",in.Num1,in.Num2,in.Num1/in.Num2)
-        return &calc.DivReply{Result:(in.Num1/in.Num2)}, nil
+func (s *server) Div(ctx context.Context, in *calc.DivRequest) (*calc.DivReply, error) {
+	if 0 == in.Num2 {
+		return nil, errors.New("divisor is 0")
+	}
+	fmt.Println("%v / %v = %v", in.Num1, in.Num2, in.Num1/in.Num2)
+	return &calc.DivReply{Result: (in.Num1 / in.Num2)}, nil
+}
+
+func (s *server) Login(stream calc.Calc_LoginServer) error {
+	for {
+		loginRequest, err := stream.Recv()
+		if err == io.EOF {
+			return stream.SendAndClose(&calc.LoginReply{
+				Wellcome: "Wellcome " + calc.LoginRequest(*loginRequest).Name + "!",
+			})
+		}
+		if err != nil {
+			return err
+		}
+	}
 }
 
 func main() {
-    lis, err := net.Listen("tcp", port)
-    if err != nil {
-        log.Fatalf("failed to listen: %v", err)
-    }
-    s := grpc.NewServer()
-    calc.RegisterCalcServer(s, &server{})
-    // Register reflection service on gRPC server.
-    reflection.Register(s)
-    if err := s.Serve(lis); err != nil {
-        log.Fatalf("failed to serve: %v", err)
-    }
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	calc.RegisterCalcServer(s, &server{})
+	// Register reflection service on gRPC server.
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
